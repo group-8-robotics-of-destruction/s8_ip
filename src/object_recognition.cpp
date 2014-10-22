ca0dc5517f2fcdfd6ccd383bfd86362400e887d9
Unnar <unnar@unnar-Satellite-L650.(none)>
2014-10-22 10:25:34 +0200

    able to remove walls and floors from point cloud, updated CMakeLists and package to enable compilation

--- src/object_recognition.cpp
+++ src/object_recognition.cpp
@@ -3,7 +3,7 @@
 // PCL specific includes
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
-#include <pcl/ros/conversions.h>
+#include <pcl/conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/features/normal_3d.h>
@@ -23,67 +23,155 @@
 #define TOPIC_POINT_CLOUD   		"/camera/depth/points"
 #define TOPIC_EXTRACTED_OBJECTS		"/s8/extractedObjects"
 
+// PARAMETERS
+#define PARAM_FILTER_X_NAME				"filter_x"
+#define PARAM_FILTER_X_DEFAULT			0.1
+#define PARAM_FILTER_Z_NAME				"filter_z"
+#define PARAM_FILTER_Z_DEFAULT			1.0
+#define PARAM_FILTER_Y_NAME				"filter_y"
+#define PARAM_FILTER_Y_DEFAULT			0.4
+#define PARAM_SEG_DISTANCE_NAME			"seg_distance"
+#define PARAM_SEG_DISTANCE_DEFAULT		0.01
+#define PARAM_SEG_PERCENTAGE_NAME		"seg_percentage"
+#define PARAM_SEG_PERCENTAGE_DEFAULT	0.2
+
 typedef pcl::PointXYZ PointT;
 
-class ObjectRecognition : public s8::Node {
+class ObjectRecognition : public s8::Node 
+{
+	const int hz;
+
 	ros::Subscriber point_cloud_subscriber;
 	ros::Publisher point_cloud_publisher;
+
+	double filter_x, filter_y, filter_z;
+	double seg_distance, seg_percentage;
+
+	sensor_msgs::PointCloud2 input, output, cloud_msg;
+	//pcl::PointCloud<PointT>::Ptr cloud_filtered;
+
 public:
-	ObjectRecognition()
+	ObjectRecognition(int hz) : hz(hz)
 	{
-		//addParams();
+		add_params();
 		//printParams();
+		//pcl::PointCloud<PointT>::Ptr tmp (new pcl::PointCloud<PointT>);
+		//cloud_filtered = tmp;
+
 		point_cloud_subscriber = nh.subscribe(TOPIC_POINT_CLOUD, BUFFER_SIZE, &ObjectRecognition::point_cloud_callback, this);
 		point_cloud_publisher  = nh.advertise<sensor_msgs::PointCloud2> (TOPIC_EXTRACTED_OBJECTS, BUFFER_SIZE);
 	}
-private:
-	void point_cloud_callback(const sensor_msgs::PointCloud2ConstPtr& input)
-	{
+
+	void update() {
+		//pcl::PointCloud<PointT>::Ptr cloud (new pcl::PointCloud<PointT>);
 		pcl::PointCloud<PointT>::Ptr cloud (new pcl::PointCloud<PointT>);
-		pcl::PointCloud<PointT>::Ptr cloud_filtered (new pcl::PointCloud<PointT>);
-		pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
-		/*
-		pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-		pcl::ModelCoefficients coefficients;
-
-		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZ> ());
-		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
-		pcl::PassThrough<PointT> pass;
-		pcl::ExtractIndices<PointT> extract;
+		//pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
 		
-		pcl::fromROSMsg (*input, *cloud);
+		pcl::fromROSMsg (input, *cloud);
 
-		// Build a passthrough filter to remove spurious NaNs
-		pass.setInputCloud (cloud);
-		pass.setFilterFieldName ("z");
-		pass.setFilterLimits (0.3, 1.0);
-		pass.filter (*cloud_filtered);
+		filterCloud(cloud);
+		cloud = segmentCloud(cloud);
 
-		pass.setInputCloud (cloud_filtered);
-		pass.setFilterFieldName ("x");
-		pass.setFilterLimits (-0.1, 0.1);
-		pass.filter (*cloud_filtered);
+        cloudPublish(cloud);
+    }
 
-		pass.setInputCloud (cloud_filtered);
-		pass.setFilterFieldName ("y");
-		pass.setFilterLimits (0, 0.4);
-		pass.filter (*cloud_filtered);
-
-		sensor_msgs::PointCloud2 output;
-  		pcl::toROSMsg(*cloud_filtered, output);
-  		pub.publish (output);
-  		*/
+private:
+	void cloudPublish(pcl::PointCloud<PointT>::Ptr cloud_pub)
+	{
+		pcl::toROSMsg(*cloud_pub, output);
+		point_cloud_publisher.publish (output);
 	}
+
+	void filterCloud(pcl::PointCloud<PointT>::Ptr cloud_filter)
+    {
+    	// Build a passthrough filter to remove spurious NaNs
+		pcl::PassThrough<PointT> pass;
+
+		pass.setInputCloud (cloud_filter);
+		pass.setFilterFieldName ("z");
+		pass.setFilterLimits (0.3, filter_z);
+		pass.filter (*cloud_filter);
+
+		pass.setInputCloud (cloud_filter);
+		pass.setFilterFieldName ("x");
+		pass.setFilterLimits (-filter_x, filter_x);
+		pass.filter (*cloud_filter);
+
+		pass.setInputCloud (cloud_filter);
+		pass.setFilterFieldName ("y");
+		pass.setFilterLimits (0, filter_y);
+		pass.filter (*cloud_filter);
+    }
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr segmentCloud(pcl::PointCloud<PointT>::Ptr cloud_seg)
+    {
+    	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZ> ());
+		pcl::ModelCoefficients::Ptr coeff (new pcl::ModelCoefficients);
+		pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+
+		pcl::SACSegmentation<pcl::PointXYZ> seg;
+		pcl::ExtractIndices<PointT> extract;
+
+		seg.setOptimizeCoefficients (true);
+		seg.setModelType (pcl::SACMODEL_PLANE);
+		seg.setMethodType (pcl::SAC_RANSAC);
+		seg.setDistanceThreshold (seg_distance);
+
+		int i = 0, nr_points = (int) cloud_seg->points.size ();
+		// While 20% of the original cloud is still there
+		while (cloud_seg->points.size () > seg_percentage * nr_points && i < 10)
+		{
+			//seg.setInputCloud (cloud);
+			seg.setInputCloud (cloud_seg);
+			seg.segment (*inliers, *coeff); 
+			if (inliers->indices.size () == 0)
+			{
+				std::cerr << "Could not estimate a planar model for the given dataset." << std::endl;
+				break;
+			}
+			if(inliers->indices.size() < nr_points/10){
+				i++;
+				std::cerr << i << "To small" << std::endl;
+				continue;
+			}
+			//ROS_INFO("%d, %d",i, (int)inliers->indices.size());
+			// Extract the planar inliers from the input cloud
+			extract.setInputCloud (cloud_seg);
+			extract.setIndices (inliers);
+			extract.setNegative (true);
+			extract.filter (*cloud_plane);
+			ROS_INFO("%d, %d",(int)cloud_seg->points.size (), (int)cloud_plane->points.size ());
+			cloud_seg.swap (cloud_plane);
+			i++;
+		}
+		return cloud_seg;
+		//cloudPublish(cloud_seg);
+    }
+
+	void point_cloud_callback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
+	{
+		input = *cloud_msg;
+	}
+
+    void add_params() 
+    {
+        add_param(PARAM_FILTER_X_NAME, filter_x, PARAM_FILTER_X_DEFAULT);
+        add_param(PARAM_FILTER_Y_NAME, filter_y, PARAM_FILTER_Y_DEFAULT);
+        add_param(PARAM_FILTER_Z_NAME, filter_z, PARAM_FILTER_Z_DEFAULT);
+        add_param(PARAM_SEG_DISTANCE_NAME, seg_distance, PARAM_SEG_DISTANCE_DEFAULT);
+        add_param(PARAM_SEG_PERCENTAGE_NAME, seg_percentage, PARAM_SEG_PERCENTAGE_DEFAULT);
+    }
 };
 
 int main(int argc, char **argv) {
+    
     ros::init(argc, argv, NODE_NAME);
 
-    ObjectRecognition recognizer();
+    ObjectRecognition recognizer(HZ);
     ros::Rate loop_rate(HZ);
 
     while(ros::ok()) {
-        //recognizer.update();
+        recognizer.update();
         ros::spinOnce();
         loop_rate.sleep();
     }
